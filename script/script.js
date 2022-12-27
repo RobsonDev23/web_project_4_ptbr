5e77badb4c49e1b1e48bd4ad6af5c6ef841e3791
Robson Ameida <robsondealmeida88@hotmail.com>
2022-12-27 15:51:33 -0300

    Enviando Projeto

--- script/script.js
+++ script/script.js
@@ -1,36 +1,36 @@
 function startModal(classModal) {
-    const modal = document.querySelector("#modalStart");
-    modal.classList.add("display");
-    modal.addEventListener("click", function (e) {
-      if (
-        e.target.id == classModal ||
-        e.target.className == "modal__close" ||
-        e.target.className == "modal__salvar"
-      ) {
-        modal.classList.remove("display");
-      }
-    });
-  }
-  
-  const button = document.querySelector(".profile__edit");
-  button.addEventListener("click", function () {
-    startModal("modalStart");
+  const modal = document.querySelector("#modalContainer");
+  modal.classList.add("display");
+  modal.addEventListener("click", function (e) {
+    if (
+      e.target.id == classModal ||
+      e.target.className == "modal__close" ||
+      e.target.className == "modal__salvar"
+    ) {
+      modal.classList.remove("display");
+    }
   });
-  
-  const formElement = document.querySelector(".modal__form");
-  
-  function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-  
-    const nameInput = document.querySelector("#subtitle");
-    const jobInput = document.querySelector("#name");
-    const title = document.querySelector(".profile__title");
-    const text = document.querySelector(".profile__text");
-  
-    const nameValue = nameInput.value;
-    const jobValue = jobInput.value;
-  
-    title.textContent = nameValue;
-    text.textContent = jobValue;
-  }
-  formElement.addEventListener("submit", handleProfileFormSubmit);
+}
+
+const button = document.querySelector(".profile__edit");
+button.addEventListener("click", function () {
+  startModal("modalContainer");
+});
+
+const formElement = document.querySelector(".modal__form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const nameInput = document.querySelector("#name");
+  const aboutInput = document.querySelector("#about");
+  const title = document.querySelector(".profile__title");
+  const subtitle = document.querySelector(".profile__subtitle");
+ 
+  const nameValue = nameInput.value;
+  const aboutValue = aboutInput.value;
+
+  title.textContent = nameValue;
+  text.textContent = aboutValue;
+}
+formElement.addEventListener("submit", handleProfileFormSubmit);
